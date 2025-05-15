db9fce2f2b5838d2440810efd5f8756e00645f2b
UNICK J <jeffreyodeloui@gmail.com>
2025-05-14 20:22:55 -0400

    Exercise Completed

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,27 @@
 {
     public class Program
     {
+
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        static string Add(int num1, int num2, bool info)
+        {
+            return info == true && num1 + num2 == 1 ? $"{num1 + num2} dollar" :
+                info == true ? $"{num1 + num2} dollars" : 
+                $"{num1 + num2}";
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($" {Add(5, 7)}\n {Add(5.6, 6.7)}\n {Add(3, 4, true)}\n " +
+                              $"{Add(1, 0, true)}\n {Add(78, 89, false)}");
         }
     }
 }
